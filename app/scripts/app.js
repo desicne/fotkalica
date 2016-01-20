1e77a7efde761ff3ffb958550b527b1a0e803e85
desicne <desicne@gmail.com>
2016-01-21 00:30:12 +0100

    Transition of all views complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,59 @@
+function homeView(){
+    var pon = document.getElementById("ponudaContent");
+    var pit = document.getElementById("pitanjaContent");
+    var kon = document.getElementById("kontaktContent");
+
+    pon.style.visibility = "hidden";
+    pit.style.visibility = "hidden";
+    kon.style.visibility = "hidden";
+}
+
+
 function getContentP() {
     
 	var pon = document.getElementById("ponudaContent");
+    var pit = document.getElementById("pitanjaContent");
+    var kon = document.getElementById("kontaktContent");
 
-    if (pon.style.visibility === "hidden") {
-        pon.style.visibility = "visible";
+    pit.style.visibility = "hidden";
+    kon.style.visibility = "hidden";
+
+    if (pon.style.visibility === "visible") {
+        pon.style.visibility = "hidden";
     } else {
-    	pon.style.visibility = "hidden";
+    	pon.style.visibility = "visible";
     }
 };
 
 function getContentPt() {
     
-	var pit = document.getElementById("pitanjaContent");
+	var pon = document.getElementById("ponudaContent");
+    var pit = document.getElementById("pitanjaContent");
+    var kon = document.getElementById("kontaktContent");
 
-    if (pit.style.visibility === "hidden") {
-        pit.style.visibility = "visible";
+    pon.style.visibility = "hidden";
+    kon.style.visibility = "hidden";
+
+    if (pit.style.visibility === "visible") {
+        pit.style.visibility = "hidden";
     } else {
-    	pit.style.visibility = "hidden";
+    	pit.style.visibility = "visible";
+    }
+};
+
+function getContentK() {
+    
+    var kon = document.getElementById("kontaktContent");
+    var pon = document.getElementById("ponudaContent");
+    var pit = document.getElementById("pitanjaContent");
+
+    pit.style.visibility = "hidden";
+    pon.style.visibility = "hidden";
+
+
+    if (kon.style.visibility === "visible") {
+        kon.style.visibility = "hidden";
+    } else {
+        kon.style.visibility = "visible";
     }
 };
