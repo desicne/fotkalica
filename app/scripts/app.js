f6c3c7ce213a4f2aaff798015f1a13e7c14b53c5
desicne <desicne@gmail.com>
2016-01-24 20:53:13 +0100

    Menu animations all in one, with good state changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,28 +9,85 @@ function homeView(){
 }
 // NEW FUNCTION FOR THE MENU
 
-function myFunction() {
+function showMenu(menuItem) {
     
-    var menuOne = document.getElementById("myDIV");
+    var nameOne = 'ponude';
+    var nameTwo = 'pitanja';
+    var nameThree = 'kontakt';
 
-    if(menuOne.style.visibility === "visible"){
-        menuOne.style.WebkitAnimation = "hideMove 2s 1";
-        menuOne.style.animation = "hideMove 2s 1";
-        menuOne.style.left = "0";
-        setTimeout(function(){
-            menuOne.style.visibility = "hidden";   
-            menuOne.style.left = "20%"; 
-        }, 2000);
+    if (menuItem == nameOne){
+        console.log('Ponude su kliknute');
+        var main = document.getElementById("ponude");
+        var siblingOne = document.getElementById("pitanja");
+        var siblingTwo = document.getElementById("kontakt");
+    } else if (menuItem == nameTwo){
+        console.log('Pitanja su kliknuta');
+        var main = document.getElementById("pitanja");
+        var siblingOne = document.getElementById("ponude");
+        var siblingTwo = document.getElementById("kontakt");
+    } else if (menuItem == nameThree){
+        console.log('Kontakt je kliknut');
+        var main = document.getElementById("kontakt");
+        var siblingOne = document.getElementById("pitanja");
+        var siblingTwo = document.getElementById("ponude");
     } else {
-        menuOne.style.visibility = "visible"; 
-        menuOne.style.WebkitAnimation = "mymove 2s 1";
-        menuOne.style.animation = "mymove 2s 1";
-        menuOne.style.right = "0";
+        console.log('Nesto nije bas dobro, kao da nista nije kliknuto');
+    };
+
+    if(main.style.visibility === "visible")
+        {
+            console.log("Usao u petlju da je izabrani meni vidljiv");
+            
+            main.style.WebkitAnimation = "hideMove 2s 1";
+            main.style.animation = "hideMove 2s 1";
+            main.style.left = "0";
+            
+            setTimeout(function(){
+                console.log("sakriva ga - glavni izabrani meni, sa tajmautom");
+                main.style.visibility = "hidden";   
+                main.style.left = "20%"; 
+            }, 1000);
+    } else {
+
+        console.log("Izabrani meni nije vidljiv , prosao inicijalno ok");
+
+        if(siblingOne.style.visibility === "visible" || siblingTwo.style.visibility === "visible"){
+
+            console.log("Usao u dodatnu petlju, drugi element neki je otvoren, sklanja ga animacijom");
+
+            siblingOne.style.WebkitAnimation = "hideMove 1s 1";
+            siblingOne.style.animation = "hideMove 1s 1";
+            siblingOne.style.left = "0";
+
+            siblingTwo.style.WebkitAnimation = "hideMove 1s 1";
+            siblingTwo.style.animation = "hideMove 1s 1";
+            siblingTwo.style.left = "0";
+            
+            setTimeout(function(){
+
+                console.log("Menja drugom elementu visibility na hidden, sa tajmautom");
+
+                siblingOne.style.visibility = "hidden";   
+                siblingOne.style.left = "20%";
+                siblingTwo.style.visibility = "hidden";   
+                siblingTwo.style.left = "20%"; 
+            }, 1000);
+        } else {
+            console.log("Drugi meni nijedan nije vidljiv");
+        
+        };
+
+        console.log("Stavlja visibility izabranog elementa na vidljiv, i animaciju pokrece");
+
+        main.style.WebkitAnimation = "mymove 2s 1";
+        main.style.animation = "mymove 2s 1";
+        main.style.right = "0";
+        main.style.visibility = "visible";
     } 
 };
 
 
-
+/*
 
 function getContentP() {
     
@@ -79,4 +136,4 @@ function getContentK() {
     } else {
         kon.style.visibility = "visible";
     }
-};
+};*/
